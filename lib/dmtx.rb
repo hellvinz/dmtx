aa6404326b2c6c0787be3b8969f7483d10fd69af
Étienne Barrié <etienne@tigerlilyapps.com>
2012-07-10 19:17:49 +0200

    Create a new DmtxEncode each time we write
    It looks like calling dmtxEncodeDataMatrix more than once on the same
    DmtxEncode leaks memory.

--- lib/dmtx.rb
+++ lib/dmtx.rb
@@ -10,23 +10,29 @@ class Dmtx
 
   def initialize(text = nil)
     @text = text
-    @enc = DmtxLib.dmtxEncodeCreate()
-    DmtxLib.dmtxEncodeSetProp(@enc, :DmtxPropPixelPacking, :DmtxPack24bppRGB)
-    DmtxLib.dmtxEncodeSetProp(@enc, :DmtxPropSizeRequest, :DmtxSymbolSquareAuto)
   end
 
   def write
     raise NoText, "please provide a text to encode" unless text
     raise NoFile, "please provide an output file" unless file
+    chunky_png_image.save(file, :fast_rgb)
+  end
 
-    DmtxLib.dmtxEncodeDataMatrix(@enc, text.length, text)
-    dmtx_encode = DmtxLib::DmtxEncode.new(@enc)
+private
+
+  def chunky_png_image
+    encoder = DmtxLib.dmtxEncodeCreate()
+    DmtxLib.dmtxEncodeSetProp(encoder, :DmtxPropPixelPacking, :DmtxPack24bppRGB)
+    DmtxLib.dmtxEncodeSetProp(encoder, :DmtxPropSizeRequest, :DmtxSymbolSquareAuto)
+
+    DmtxLib.dmtxEncodeDataMatrix(encoder, text.length, text)
+    dmtx_encode = DmtxLib::DmtxEncode.new(encoder)
     computed_width = DmtxLib.dmtxImageGetProp(dmtx_encode[:image], :DmtxPropWidth)
     computed_height = DmtxLib.dmtxImageGetProp(dmtx_encode[:image], :DmtxPropHeight)
 
     dmtx_image = DmtxLib::DmtxImage.new(dmtx_encode[:image])
 
-    image = ChunkyPNG::Image.from_rgb_stream(computed_width, computed_height, dmtx_image[:pxl].read_string_length(computed_height*computed_width*3))
-    image.save(file, :fast_rgb)
+    ChunkyPNG::Image.from_rgb_stream(computed_width, computed_height, dmtx_image[:pxl].read_string_length(computed_height*computed_width*3))
   end
+
 end
